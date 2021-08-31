7afa4975c36880b0c207882959ebecbfaad6919f
Breno Vergílio <brenoverglio@pop-os.localdomain>
2021-08-30 21:02:41 -0400

    adding nodemon and setting environment variables

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+});
+
